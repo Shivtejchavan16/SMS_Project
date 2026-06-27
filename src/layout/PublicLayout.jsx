6b8aa416597c772d99848464d39c07faef3b7e63
Shivtej Chavan <shivtejchavan696@gmail.com>
2026-06-27 08:32:39 +0530

    Setup Routing Architecture

--- src/layout/PublicLayout.jsx
+++ src/layout/PublicLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const PublicLayout = () => {
   return (
-    <div>
+    <>
       <h1>Public Layout</h1>
-    </div>
+
+      <Outlet/>
+    </>
   )
 }
 
