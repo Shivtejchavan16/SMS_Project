9c3e3ef3d5dce50be65e4ba4ffa932236e521def
unknown <sastesujata2005@gmail.com>
2026-06-28 12:29:24 +0530

    completed Courses pages

---
+++ src/mockdata/courses.js
@@ -0,0 +1,16 @@
+const courses = [
+  {
+  id: 1,
+  courseName: "React JS",
+  courseCode: "CS101",
+  duration: "6 Months",
+  department: "Computer Science",
+  assignedTeacher: "Aarav Patil",
+  status: "Active",
+  students: 120,
+  description:
+    "Learn React fundamentals, hooks, routing, state management, API integration, and build real-world projects."
+}
+];
+
+export default courses;
